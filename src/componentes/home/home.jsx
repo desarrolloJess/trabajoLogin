bdf4e89e969bec33573c6ba79a2000791f9e1b1e
JESSICA <95001228+desarrolloJess@users.noreply.github.com>
2024-11-18 13:34:09 -0600

    Correcciones

--- src/componentes/home/home.jsx
+++ src/componentes/home/home.jsx
@@ -32,19 +32,20 @@ const HomeUsuarios = () => {
       <div className="areaCard">
         {/* Card de Usuario */}
         <div className="card" style={{ width: '35rem', margin: '0 auto' }}>
-          <img
+          <img 
             src={user ? user.image : "https://www.clarin.com/2024/04/25/HKCtyMwSF_2000x1500__1.jpg"}
-            className="card-img-top"
+            className="rounded-circle" 
             alt="Imagen de usuario"
-            style={{ width: '100%', height: '350px', objectFit: 'cover' }}
+            style={{ width: '200px', height: '200px', objectFit: 'cover', marginLeft : '30%'}}
           />
+
           <div className="card-body">
-            <h5 className="card-title">{user ? user.name : 'Juan Pérez'}</h5>
+            <h5 className="card-title" style={{ textTransform: 'uppercase' }}>
+              {user ? user.firstName +" "+ user.lastName: 'Juan Pérez'}
+            </h5>
             <p className="card-text">{user ? user.job : 'Desarrollador Web'}</p>
             <ul className="list-unstyled">
-              <li><strong>Correo:</strong> {user ? user.email : 'juanperez@example.com'}</li>
-              <li><strong>Teléfono:</strong> {user ? user.phone : '+123 456 7890'}</li>
-              <li><strong>Ubicación:</strong> {user ? user.location : 'Ciudad de México'}</li>
+              <li><strong>Género: </strong> {user ? user.gender : ''}</li>
             </ul>
           </div>
         </div>
